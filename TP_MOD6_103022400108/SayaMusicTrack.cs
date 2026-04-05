9eb5d29c42b989835c36ec024b12e98bc5c90036
Ardolski <ardisetyapermana@gmail.com>
2026-04-05 11:07:09 +0700

    menambahkan implementasi design by contract

--- TP_MOD6_103022400108/SayaMusicTrack.cs
+++ TP_MOD6_103022400108/SayaMusicTrack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace TP_MODUL6_NIM
 {
@@ -10,6 +11,9 @@ namespace TP_MODUL6_NIM
 
         public SayaMusicTrack(string title)
         {
+            Debug.Assert(title != null, "Judul tidak boleh null");
+            Debug.Assert(title.Length <= 100, "Judul maksimal 100 karakter");
+
             this.title = title;
             Random random = new Random();
             this.id = random.Next(10000, 99999);
@@ -18,12 +22,24 @@ namespace TP_MODUL6_NIM
 
         public void IncreasePlayCount(int count)
         {
-            this.playCount += count;
+            
+            Debug.Assert(count <= 10000000, "Input penambahan maksimal 10.000.000");
+
+            try
+            {
+                {
+                    this.playCount += count;
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Terjadi kesalahan: Play count melebihi batas maksimum (Overflow)!");
+            }
         }
 
         public void PrintTrackDetails()
         {
-            Console.WriteLine($"ID: {this.id}, Title: {this.title}, Play Count: {this.playCount}");
+            Console.WriteLine($"ID: {id} | Title: {title} | Play Count: {playCount}");
         }
     }
 }
